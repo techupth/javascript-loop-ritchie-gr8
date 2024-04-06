0593df4b6f7d6219ed847a93dc0659d0294bc896
ritchie-gr8 <rit.yotsathon@gmail.com>
2024-04-06 20:21:48 +0700

    feat: add answers

--- ex-3.js
+++ ex-3.js
@@ -2,5 +2,12 @@
 let scores = [100, 20, 3, 1000];
 let minScore;
 // Start coding here
+minScore = Number.MAX_VALUE
+
+for (let i = 0; i < scores.length; i++) {
+    if (scores[i] < minScore) {
+        minScore = scores[i]
+    }
+}
 
 console.log(minScore);
